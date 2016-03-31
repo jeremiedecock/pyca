53d1082ff7629be6a8a3ed696b6e5b55f6185851
Jérémie Decock <jd.jdhp@gmail.com>
2016-04-01 00:11:14 +0200

    Add a script to automatically build the Debian package.

---
+++ debian8-dist.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+NAME=python3-ca
+#VERSION=$(grep "__version__" ca/__init__.py | cut --delimiter="'" -f2)
+VERSION=$(python -c "print(__import__('ca').__version__)")
+DIST_DIR=dist
+
+rm -rf debian
+
+# TODO
+mkdir -p     debian/usr/local/lib/python3.0/dist-packages
+cp -r ca     debian/usr/local/lib/python3.0/dist-packages
+chmod 644    $(find debian/usr/local/lib -type f)
+
+mkdir -p      "debian/usr/share/doc/$NAME/"
+cp LICENSE    "debian/usr/share/doc/$NAME/copyright"
+chmod 644     "debian/usr/share/doc/$NAME/copyright"
+
+mkdir -p debian/DEBIAN
+
+# section list : http://packages.debian.org/stable/
+cat > debian/DEBIAN/control << EOF
+Package: $NAME
+Version: $VERSION
+Section: libs
+Priority: optional
+Maintainer: Jérémie DECOCK <jd.jdhp@gmail.com>
+Architecture: all
+Depends: python (>= 3.0)
+Description: A lightweight cellular automata framework
+EOF
+
+fakeroot dpkg-deb -b debian
+
+mkdir -p "$DIST_DIR"
+mv debian.deb "$DIST_DIR/${NAME}_${VERSION}_all.deb"
