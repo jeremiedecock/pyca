07c423578679dfc0211e9ac2ff7abe3d37f1baed
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-02 15:48:46 +0200

    Add comments.

--- ca/rules/conway.py
+++ ca/rules/conway.py
@@ -44,6 +44,11 @@ class Rules:
             for idy in range(current_state.height):
                 num_live_neighbors = sum(self.get_neighbors(current_state, idx, idy))
 
+                # Game of life rules (https://en.wikipedia.org/wiki/Conway's_Game_of_Life):
+                # - Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+                # - Any live cell with two or three live neighbours lives on to the next generation.
+                # - Any live cell with more than three live neighbours dies, as if by over-population.
+                # - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                 if (current_state[idy][idx] == 0) and (num_live_neighbors == 3):
                     next_state[idy][idx] = 1
                 elif (current_state[idy][idx] == 1) and (2 <= num_live_neighbors <= 3):
